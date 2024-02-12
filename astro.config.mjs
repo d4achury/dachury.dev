25b98a020d8deffdc0a2f878f6d00550dc746978
d4chury <15achury02@gmail.com>
2024-02-11 22:17:29 -0500

    Add Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const SERVER_PORT = 4321;
+const URL= "https://d4achury.github.io"
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: URL,
+  base: '/d4achury.dev',
 });
